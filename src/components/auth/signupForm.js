c04dde31d35f883056464c6c0402dcdd427117f1
MatheStine <cmturner072@gmail.com>
2021-04-19 16:59:03 -0500

    built headernavbar reducer

--- src/components/auth/signupForm.js
+++ src/components/auth/signupForm.js
@@ -10,84 +10,72 @@ import history from '../../history';
 class SignUpForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
-        const links = [
+        const info = [
             {
                 _id: 0,
-                title: 'Not Registered? Create account here',
-                onClick: () => history.push('/signup')
+                title: 'At least 6 characters',
+
             },
             {
                 _id: 1,
-                title: 'Forgot account email?',
-                onClick: () => console.log('forgot email')
+                title: 'At least one number',
+
             },
             {
                 _id: 2,
-                title: 'Forgot password?',
-                onClick: () => console.log('forgot password')
+                title: 'At least one symbol',
+
             }
         ]
         return (
             <form onSubmit={handleSubmit} className={`${className} sign-up-form`}>
-                <Field className='sign-up-form__name' 
-                type='name' 
-                title='Name' 
-                placeholder='Name' 
-                name='name' 
+                <Field className='sign-up-form__name'
+                type='name'
+                title='Name'
+                placeholder='Name'
+                name='name'
+                component={FormInput}/>
+                <Field className='sign-up-form__email'
+                type='email'
+                title='Email'
+                placeholder='Email'
+                name='email'
+                component={FormInput}/>
+                <Field className='sign-up-form__password'
+                type='password'
+                title='Password'
+                placeholder='Password'
+                name='password'
+                component={FormInput}/>
+                <Field className='sign-up-form__confirm-password'
+                type='password'
+                title='Confirm Password'
+                placeholder='Confirm Password'
+                name='confirm'
                 component={FormInput}/>
 
-                <Field className='sign-up-form__email' 
-                type='email' 
-                title='Email' 
-                placeholder='Email' 
-                name='email' 
-                component={FormInput}/> 
-                 
-
-                <Field className='sign-up-form__password' 
-                type='password' 
-                title='Password' 
-                placeholder='Password' 
-                name='password' 
-                component={FormInput}/>
-
-                <Field className='sign-up-form__confirm-password' 
-                type='password' 
-                title='Confirm Password' 
-                placeholder='Confirm Password' 
-                name='confirm' 
-                component={FormInput}/>
-
-                <div className='sign-up-form__line'></div> 
-
-                <Field className='sign-up-form__back'
-                onClick={() => console.log('tryin to go back')} 
-                type='button' 
-                title='Back' 
-                name='back'
-                short={true} 
-                component={FormButton}/>
-
+               <div className='sign-up-form__line'></div>
                 <Field className='sign-up-form__login'
-                onClick={() => console.log('trying to submit')} 
-                type='submit' 
-                title='Login' 
+                onClick={() => history.push('/account')}
+                type='submit'
+                title='Create Account'
                 name='login'
-
-                component={FormButton}/>    
-                <Details 
-                    className='sign-up-form__details' 
-                    title='QuickLinks' 
-                    links={links}
-
-                />
+                component={FormButton}/>
+                <Field className='sign-up-form__back'
+                onClick={() => history.push('/signin')}
+                type='button'
+                title='Back'
+                name='back'
+                short={true}
+                component={FormButton}/>
+                <Details className='sign-up-form__details' title='Password Recquirements' info={info}/>
             </form>
         )
     }
 }
-SignUpForm = reduxForm({
-    form: 'SignIn'
-})(SignUpForm);
 
+SignUpForm = reduxForm({
+    form: 'SignUpForm'
+})(SignUpForm);
 
 export default SignUpForm;
