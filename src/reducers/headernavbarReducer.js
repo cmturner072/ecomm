2a6e6a109fc082f2bb265eda117d2ec656710a98
MatheStine <cmturner072@gmail.com>
2021-04-22 16:53:45 -0500

    finished navbar feature

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -1,17 +1,16 @@
 import {
     SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
+    SET_NAVBAR_LINKS,
+    CHANGE_NAVBAR_ACTIVE
 } from '../actions/types';
-
 
 const INITIAL_STATE = {
     headerLinks: [],
     navbarLinks: []
-
 }
 
 export default function(state = INITIAL_STATE, action) {
-    switch(action.type) {
+    switch (action.type) {
         case SET_HEADER_LINKS:
             return {
                 ...state,
@@ -21,6 +20,18 @@ export default function(state = INITIAL_STATE, action) {
             return {
                 ...state,
                 navbarLinks: action.payload
+            }
+        case CHANGE_NAVBAR_ACTIVE: 
+            const navbarLinks = state.navbarLinks.map(link => {
+                link.active = false;
+                if(link._id == action.payload) {
+                    link.active = true;
+                }
+                return link;
+            })
+            return {
+                ...state,
+                navbarLinks
             }
         default: return state;
     }
